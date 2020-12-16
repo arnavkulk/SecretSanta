87e008cc8d491dca42a5b8ce1278f560b618f2cc
Arnav Kulkarni <arnavyk@gmail.com>
2020-12-16 14:35:15 -0800

    testing env vars with gh pages

--- secret-santa-website/src/App.tsx
+++ secret-santa-website/src/App.tsx
@@ -1,23 +1,9 @@
 import React from "react";
+import { db, auth } from "./firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(`CURRENT USER: ${auth.currentUser}`);
+  return <div>HELLO WORLD</div>;
 }
 
 export default App;
