848d62e364ce44c4c12db1c8f02612ca2481fbbb
Arnav Kulkarni <arnavyk@gmail.com>
2020-12-15 21:41:43 -0800

    deleted extra files

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
-// Your web app's Firebase configuration
-var firebaseConfig = {
+
+const firebaseConfig = {
   apiKey: "AIzaSyDYdwZc1XvhmTedGOB2ALV1D6HjzqeQfrw",
   authDomain: "secrets-91dd7.firebaseapp.com",
   projectId: "secrets-91dd7",
@@ -9,9 +9,10 @@ var firebaseConfig = {
   appId: "1:200284184264:web:8ef308bb269c0f86850ffd",
   measurementId: "G-E2E3V7T4FJ",
 };
-firebase.initializeApp({
-  apiKey: "AIzaSyDYdwZc1XvhmTedGOB2ALV1D6HjzqeQfrw",
-  authDomain: "secrets-91dd7.firebaseapp.com",
-  projectId: "secrets-91dd7",
-});
-export default firebase;
+
+let app = firebase.initializeApp(firebaseConfig);
+
+export let db = app.firestore()
+export let auth = app.auth()
+
+export default app;
